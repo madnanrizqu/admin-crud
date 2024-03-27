a66e4cdd8edd7b589365b4ca3b9f33ee9cbd0308
rizquadnan <madnanrizqullah@gmail.com>
2024-03-27 11:35:55 +0700

    add basic layout

--- src/routes/PrivateLayout.tsx
+++ src/routes/PrivateLayout.tsx
@@ -1,9 +1,33 @@
+import { Button, Container, Flex, Text } from "@mantine/core";
 import { Outlet } from "react-router-dom";
+
+import { useAuthStore } from "@/store/auth";
+
+import classes from "./PublicLayout.module.css";
+
 export default function PrivateLayout() {
+  const authStore = useAuthStore();
+
   return (
-    <>
-      <h1>Private</h1>
-      <Outlet />
-    </>
+    <Container>
+      <header className={classes.navbar}>
+        <Flex justify="space-between" align="center">
+          <Text>CRUD Admin</Text>
+          <Flex direction="column">
+            <Text>Hi, {authStore.user?.name}</Text>
+            <Button
+              variant="outline"
+              size="xs"
+              onClick={() => authStore.clearState()}
+            >
+              Logout
+            </Button>
+          </Flex>
+        </Flex>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </Container>
   );
 }
